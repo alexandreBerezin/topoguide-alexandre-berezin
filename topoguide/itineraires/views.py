325413ce8aa8e695972a2c611e1b9270b507cdfe
Alexandre <alexandre.berezin@student.isae-supaero.fr>
2022-04-19 10:25:28 +0200

    Authentification Page created and route to index
    Creation of an athentification page at /accounts/login and add a route to index page

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Index de l'application itineraires: pour se connecteur cliquer ici : ")
