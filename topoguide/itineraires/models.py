5f03d374a3ffbab8b9a4a9c0a4456d0f3991b49b
Alexandre <alexandre.berezin@student.isae-supaero.fr>
2022-04-21 16:39:23 +0200

    Add template heritance

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -4,13 +4,40 @@ from django.contrib.auth.models import User
 
 class Itineraire(models.Model):
     title = models.CharField(max_length=200)
+    start = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    altitude_start = models.IntegerField()
+    altitude_min = models.IntegerField()
+    altitude_max = models.IntegerField()
+    
+    denivele_positif = models.IntegerField()
+    denivele_negatif = models.IntegerField()
+    duree_estimee = models.FloatField()
+    difficulte = models.IntegerField()
+    
     def __str__(self):
         return self.title
     
     
     
 class Sortie(models.Model):
+    
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     sortie_itineraire = models.ForeignKey(Itineraire,on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    duree_reelle = models.FloatField()
+    nombre_personnes = models.IntegerField()
+    groupe_experience = models.IntegerField()
+    meteo = models.CharField(max_length=200)
+    difficulte_ressentie = models.IntegerField()
+
+    
+    def __str__(self):
+
+        return self.user.username + " " +  self.sortie_itineraire.title + " " + str(self.date)[:10]
+    
+    
+    
+    
 
     
